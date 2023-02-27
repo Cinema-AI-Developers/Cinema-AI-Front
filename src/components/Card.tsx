341a0f05643d13723a95a9fb9f48c0741b6c7465
Vasiliy Razdorskiy <vrazdorsky@yandex.ru>
2023-02-27 16:03:55 +0300

    Make rating visible only if it is available #2

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,13 +11,12 @@ const Card = ({ title, imgUrl, rating, year, filmLength }: CardProps) => {
     if (rating) {
       return rating >= 6 ? 'card__rating_color_green' : 'card__rating_color_red';
     }
-    return 'card__rating_color_none';
   };
 
   return (
     <div className='card'>
       <button className='card__button'>
-        <p className={`card__rating ${checkRating(rating)}`}>{rating}</p>
+        {rating && <p className={`card__rating ${checkRating(rating)}`}>{rating}</p>}
         <img src={imgUrl} alt={title} className='card__photo' />
       </button>
       <p className='card__title'>{title}</p>

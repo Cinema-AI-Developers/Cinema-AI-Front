fd30f6bacff75298bb47d8046b232c2b0efd9132
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-03-06 17:34:12 +0300

    intermediate commit

--- src/pages/FilmPage.tsx
+++ src/pages/FilmPage.tsx
@@ -1,7 +1,10 @@
-import { useEffect } from 'react';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { useEffect, useRef } from 'react';
 import { useParams } from 'react-router';
 import { useQuery } from '@tanstack/react-query';
 import { api } from '../utlis/api';
+import ReactDOM from 'react-dom';
 
 const FilmPage = () => {
   const { id } = useParams();
@@ -12,16 +15,19 @@ const FilmPage = () => {
     isError,
   } = useQuery({
     queryKey: ['film', { id }],
-    queryFn: () => api.getFilmById(id),
+    queryFn: async () => await api.getFilmById(id),
   });
 
+  const container = React.useRef(null);
+
   useEffect(() => {
-    const script = document.createElement('script');
+    console.log(container.current);
+    const film = <div id='yohoho' data-kinopoisk={`${filmData?.filmId}`}></div>;
+    //let film = document.createElement('div');
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
     script.src = 'https://yohoho.cc/yo.js';
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
+    //ReactDOM.render(film, container);
   }, []);
 
   return filmIsLoading ? (
@@ -31,9 +37,7 @@ const FilmPage = () => {
   ) : (
     <>
       <h2 className='film__name'>{filmData.nameRu}</h2>
-      <div className='film__container'>
-        <div id='yohoho' data-title='Тайна Коко'></div>
-      </div>
+      <div className='film__container' ref={container}></div>
     </>
   );
 };

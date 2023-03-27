de09c844386eeca8abd059f9c304e86c7d95273e
Vasilii Razdorskii <vrazdorsky@yandex.ru>
2023-03-27 23:05:32 +0300

    Change filmpage structure from grid to section & aside blocks #12

--- src/pages/FilmPage.tsx
+++ src/pages/FilmPage.tsx
@@ -36,8 +36,6 @@ const FilmPage = () => {
     return `${hours} ч. ${minutes} мин.`;
   };
 
-  console.log(filmData);
-
   return filmIsLoading ? (
     <p>Loading...</p>
   ) : isError ? (
@@ -46,93 +44,100 @@ const FilmPage = () => {
     <>
       <h2 className='film__name'>{filmData.nameRu}</h2>
       <div className='film__content'>
-        <div className='film__container'>
-          <div id='yohoho' data-kinopoisk={`${id}`}></div>
-        </div>
-        <ul className='film__info'>
-          <p className='film__info-title'>
-            Информация о {filmData.type === 'TV_SERIES' ? 'сериале' : 'фильме'}:
-          </p>
+        <section className='film__main-container'>
+          <div className='film__container'>
+            <div id='yohoho' data-kinopoisk={`${id}`}></div>
+          </div>
 
-          {filmData.nameOriginal && (
-            <li className='film__info-item'>
-              <p className='film__info-name film__info-name_lang_en'>
-                Оригинальное название: {filmData.nameOriginal || filmData.nameEn}
-              </p>
-            </li>
-          )}
+          <div className='film__description-container'>
+            <p className='film__description-title'>Описание:</p>
+            <p className='film__description'>{filmData.description}</p>
+          </div>
+        </section>
 
-          {filmData.year && filmData.type === 'FILM' && (
-            <li className='film__info-item'>
-              <p className='film__info-year'>Год выпуска: {filmData.year}</p>
-            </li>
-          )}
+        <aside className='film__aside-container'>
+          <ul className='film__info'>
+            <p className='film__info-title'>
+              Информация о {filmData.type === 'TV_SERIES' ? 'сериале' : 'фильме'}:
+            </p>
 
-          {filmData.type === 'TV_SERIES' && (
-            <li className='film__info-item'>
-              <p className='film__info-year'>
-                Год выпуска: {filmData.startYear} - {filmData.endYear ? filmData.endYear : '...'}
-              </p>
-            </li>
-          )}
+            {filmData.nameOriginal && (
+              <li className='film__info-item'>
+                <p className='film__info-name film__info-name_lang_en'>
+                  Оригинальное название: {filmData.nameOriginal || filmData.nameEn}
+                </p>
+              </li>
+            )}
 
-          {filmData.countries && (
-            <li className='film__info-item'>
-              <p className='film__info-countries'>
-                Страны: <span>{filmData.countries.map((e) => e.country).join(', ')}</span>
-              </p>
-            </li>
-          )}
+            {filmData.year && filmData.type === 'FILM' && (
+              <li className='film__info-item'>
+                <p className='film__info-year'>Год выпуска: {filmData.year}</p>
+              </li>
+            )}
 
-          {filmData.genres && (
-            <li className='film__info-item'>
-              <p className='film__info-genres'>
-                Жанры: <span>{filmData.genres.map((e) => e.genre).join(', ')}</span>
-              </p>
-            </li>
-          )}
+            {filmData.type === 'TV_SERIES' && (
+              <li className='film__info-item'>
+                <p className='film__info-year'>
+                  Год выпуска: {filmData.startYear} - {filmData.endYear ? filmData.endYear : '...'}
+                </p>
+              </li>
+            )}
 
-          {filmData.type === 'FILM' && (
-            <li className='film__info-item'>
-              <p className='film__info-time'>
-                Время просмотра: {convertWatchingTime(parseInt(filmData.filmLength || '0'))}
-              </p>
-            </li>
-          )}
+            {filmData.countries && (
+              <li className='film__info-item'>
+                <p className='film__info-countries'>
+                  Страны: <span>{filmData.countries.map((e) => e.country).join(', ')}</span>
+                </p>
+              </li>
+            )}
 
-          {(filmData.ratingKinopoisk || filmData.ratingImdb) && (
-            <li className='film__info-item'>
-              <div className='film__ratings-container'>
-                {filmData.ratingKinopoisk && (
-                  <div className='film__rating-item'>
-                    <p className='film__rating-count'>
-                      {<Rating rating={filmData.ratingKinopoisk} place='filmPage' />}
-                    </p>
+            {filmData.genres && (
+              <li className='film__info-item'>
+                <p className='film__info-genres'>
+                  Жанры: <span>{filmData.genres.map((e) => e.genre).join(', ')}</span>
+                </p>
+              </li>
+            )}
 
-                    <p className='film__rating-source'>Рейтинг Кинопоиск</p>
-                  </div>
-                )}
+            {filmData.type === 'FILM' && (
+              <li className='film__info-item'>
+                <p className='film__info-time'>
+                  Время просмотра: {convertWatchingTime(parseInt(filmData.filmLength || '0'))}
+                </p>
+              </li>
+            )}
 
-                {filmData.ratingImdb && (
-                  <div className='film__rating-item'>
-                    <p className='film__rating-count'>
-                      {<Rating rating={filmData.ratingImdb} place='filmPage' />}
-                    </p>
+            {(filmData.ratingKinopoisk || filmData.ratingImdb) && (
+              <li className='film__info-item'>
+                <div className='film__ratings-container'>
+                  {filmData.ratingKinopoisk && (
+                    <div className='film__rating-item'>
+                      <p className='film__rating-count'>
+                        {<Rating rating={filmData.ratingKinopoisk} place='filmPage' />}
+                      </p>
 
-                    <p className='film__rating-source'>Рейтинг Imdb</p>
-                  </div>
-                )}
-              </div>
-            </li>
-          )}
-        </ul>
-        <div className='film__description-container'>
-          <p className='film__description-title'>Описание:</p>
-          <p className='film__description'>{filmData.description}</p>
-        </div>
-        <div className='film__user-rating-container'>
-          <p>Оценки</p>
-        </div>
+                      <p className='film__rating-source'>Рейтинг Кинопоиск</p>
+                    </div>
+                  )}
+
+                  {filmData.ratingImdb && (
+                    <div className='film__rating-item'>
+                      <p className='film__rating-count'>
+                        {<Rating rating={filmData.ratingImdb} place='filmPage' />}
+                      </p>
+
+                      <p className='film__rating-source'>Рейтинг Imdb</p>
+                    </div>
+                  )}
+                </div>
+              </li>
+            )}
+          </ul>
+
+          <div className='film__user-rating-container'>
+            <p>Оценки</p>
+          </div>
+        </aside>
       </div>
     </>
   );

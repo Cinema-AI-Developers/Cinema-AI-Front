b5c4362865d11b4cfed24f0b3a261bf90c08a6f3
uumlaut <flower.daf29@gmail.com>
2023-04-01 22:49:31 +0300

    Add working (maybe) GPT message (first iteration ofc)

---
+++ src/utlis/GPTapi.tsx
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const openai = axios.create({
+  baseURL: 'https://api.openai.com/v1/',
+  headers: { Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API}` },
+});
+
+interface AIResponse {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  usage: {
+    prompt_tokens: number;
+    completion_tokens: number;
+    total_tokens: number;
+  };
+  choices: {
+    message: {
+      role: string;
+      content: string;
+    };
+    finish_reason: string;
+    index: number;
+  }[];
+  error: {
+    message: string;
+    type: string;
+    param: number;
+    code: number;
+  };
+}
+
+async function sendMessageGPT(userMessage: string): Promise<string> {
+  const response = await openai.post<AIResponse>('/chat/completions', {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: 'Веди себя как Дэвид, помощник в подборке фильмов в приложении CinemaAI.',
+      },
+      {
+        role: 'user',
+        content: 'Я хочу посмотреть фильмы о членах',
+      },
+    ],
+  });
+  const assistantResponse = response.data.choices[0].message.content.trim();
+  return assistantResponse;
+}
+
+export const GPTapi = {
+  sendMessageGPT,
+};
+
+// const userMessage = 'Я хочу посмотреть фильм о Филлипе';
+// const assistantResponse = GPTapi.sendMessageGPT(userMessage);
+// console.log(assistantResponse);
